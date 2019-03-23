32eb5c65cb04ffac5210ff9745d4e0f94e0aa6c8
smartymoon <pp787qq@gmail.com>
2019-03-23 08:08:12 +0000

    whatever'

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::view('/', 'welcome');
 
 // 查找单词，使用频率，主要意思工具，用 倍洽进行沟通
 Route::post('words/lookup', 'WordsController@lookUp');
-//Route::get('/{star}', 'WordsController@printWords');
+// Route::get('/{star}', 'WordsController@printWords');
 
 Route::any('wechat', 'WeChatController@serve');
 
